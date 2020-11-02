26e521dd3cdbef09ebb3836333cc34ed87c42601
dvidov96 <dvidov96p@hotmail.com>
2020-11-02 20:33:03 +0100

    Creating TitlePage

---
+++ src/components/InquiryBlock/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import styles from "./style.module.css"
+
+const InquiryBlock = () => <section className={styles.inquiryBlock}></section>
+
+export default InquiryBlock
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 import HeaderFooterLayout from "../layouts/headerFooter"
-
+import TitlePage from "../modules/TitlePage"
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
-    {/* Body components here! */}
+    <TitlePage />
   </HeaderFooterLayout>
 )
 
