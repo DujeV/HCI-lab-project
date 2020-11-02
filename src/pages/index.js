809f8ec0f44aeef77414c48cf0823f7e7d646ba9
dvidov96 <dvidov96p@hotmail.com>
2020-11-02 18:03:08 +0100

    refactor to use props for .active

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import NavigationHeader from "../components/NavigationHeader"
 const IndexPage = () => (
   <main>
     <ContactBar />
-    <NavigationHeader />
+    <NavigationHeader activeTab="Contact" />
   </main>
 )
 
