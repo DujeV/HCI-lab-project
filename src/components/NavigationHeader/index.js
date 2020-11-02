11d6fb477ec84b5578204b33d0edcbdaeeb96599
dvidov96 <dvidov96p@hotmail.com>
2020-11-02 17:34:42 +0100

    Adding NavigationHeader

---
+++ src/components/NavigationHeader/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+import styles from "./style.module.css"
+
+const NavigationHeader = () => (
+  <section className={styles.navigationHeader}></section>
+)
+export default NavigationHeader
