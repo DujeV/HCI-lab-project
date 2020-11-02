3242cc91166b82e5cd32607c8c987eeb652608ac
dvidov96 <dvidov96p@hotmail.com>
2020-11-02 17:38:22 +0100

    Adding Logo

--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -1,8 +1,10 @@
 import React from "react"
-
+import Logo from "../Logo"
 import styles from "./style.module.css"
 
 const NavigationHeader = () => (
-  <section className={styles.navigationHeader}></section>
+  <section className={styles.navigationHeader}>
+    <Logo />
+  </section>
 )
 export default NavigationHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import ContactBar from "../components/ContactBar"
+import NavigationHeader from "../components/NavigationHeader"
 
 const IndexPage = () => (
   <main>
     <ContactBar />
+    <NavigationHeader />
   </main>
 )
 
