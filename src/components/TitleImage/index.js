d656021b5bbca2a0943d24f64af845824a202f8b
dvidov96 <dvidov96p@hotmail.com>
2020-11-05 19:34:03 +0100

    adding gatsby's background image

--- src/components/TitleImage/index.js
+++ src/components/TitleImage/index.js
@@ -1,17 +1,37 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
 import styles from "./style.module.css"
 
-const TitleImage = () => (
-  <section className={styles.titleImage}>
-    <div>
-      <h1>VILLA OLIVA VERDE</h1>
-      <p>
-        Isolated from the tourist crowd for a pleasant
-        <br /> and peaceful vacation
-      </p>
-    </div>
-  </section>
-)
+const TitleImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "titleImage.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <BackgroundImage
+      Tag="section"
+      className={styles.titleImage}
+      fluid={data.desktop.childImageSharp.fluid}
+      backgroundColor={`#040e18`}
+    >
+      <div>
+        <h1>VILLA OLIVA VERDE</h1>
+        <p>
+          Isolated from the tourist crowd for a pleasant
+          <br /> and peaceful vacation
+        </p>
+      </div>
+    </BackgroundImage>
+  )
+}
 
 export default TitleImage
