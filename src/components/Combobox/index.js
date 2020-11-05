e92781d6c88c8f421ef8ef32ca620638ed201ad5
dvidov96 <dvidov96p@hotmail.com>
2020-11-05 14:38:54 +0100

    Creating Combobox: Look and style

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,7 +1,19 @@
 import React from "react"
 
+import { languages } from "../../constants/const"
+import ComboboxItem from "./ComboboxItem"
 import styles from "./style.module.css"
 
-const Combobox = () => <span className={styles.combobox}>English</span>
+const Combobox = () => (
+  <div className={styles.combobox}>
+    {
+      <ul>
+        {languages.map(el => (
+          <ComboboxItem {...el} />
+        ))}
+      </ul>
+    }
+  </div>
+)
 
 export default Combobox
