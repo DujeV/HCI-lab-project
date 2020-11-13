31af80ee86df01e7565b23f5234c768efd60146c
DujeV <dvidov01@fesb.hr>
2020-11-13 18:40:45 +0100

    Upgrading GalleryContainer to accept bounds and style

--- src/components/GalleryContainer/index.js
+++ src/components/GalleryContainer/index.js
@@ -4,10 +4,13 @@ import ImagePool from "../ImagePool"
 
 import styles from "./style.module.css"
 
-const GalleryContainer = ({ title }) => (
-  <section className={styles.galleryContainer}>
+const GalleryContainer = ({ title, start, end, isGray }) => (
+  <section
+    style={{ backgroundColor: isGray ? "#f2f2f2" : "white" }}
+    className={styles.galleryContainer}
+  >
     <h1>{title}</h1>
-    <ImagePool start={1} end={4} />
+    <ImagePool start={start} end={end} />
   </section>
 )
 
